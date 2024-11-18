a48892f75b2570db2e35c533c11fa15b6a8133de
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-18 12:16:20 +0900

    fix: gameview.html->GameView.html

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
     playButton.addEventListener("click", function () {
         // 버튼 텍스트 및 동작 업데이트
         playButton.textContent = "Level 1 (2 X 2)";
-        playButton.onclick = () => window.location.href = "gameview.html?level=1";
+        playButton.onclick = () => window.location.href = "GameView.html?level=1";
 
         helpButton.textContent = "Level 2 (3 X 3)";
-        helpButton.onclick = () => window.location.href = "gameview.html?level=2";
+        helpButton.onclick = () => window.location.href = "GameView.html?level=2";
 
         // Level 3 버튼 생성
         const level3Button = document.createElement("button");
         level3Button.className = "game-button";
         level3Button.textContent = "Level 3 (4 X 4)";
-        level3Button.onclick = () => window.location.href = "gameview.html?level=3";
+        level3Button.onclick = () => window.location.href = "GameView.html?level=3";
 
         // 버튼 컨테이너에 추가
         buttonContainer.appendChild(level3Button);
